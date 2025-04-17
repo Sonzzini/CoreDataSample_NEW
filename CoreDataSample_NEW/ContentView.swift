1204e54775bf35d035fcf464b6ea1138c33a79a7
Sonzzini <paulo.sonzzini@gmail.com>
2025-04-17 10:40:20 -0300

    Initial Commit

---
+++ CoreDataSample_NEW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreDataSample_NEW
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreDataSample_NEW/CoreDataSample_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSample_NEWApp.swift
+//  CoreDataSample_NEW
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSample_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
